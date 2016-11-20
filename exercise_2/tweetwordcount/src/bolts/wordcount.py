404bca9955b52c1e169b1c272683c2d08ec5077a
chqngh-berkeley <chqngh@berkeley.edu>
2016-11-19 22:10:55 -0800

    updates

--- exercise_2/tweetwordcount/src/bolts/wordcount.py
+++ exercise_2/tweetwordcount/src/bolts/wordcount.py
@@ -8,11 +8,6 @@ conn = psycopg2.connect(database="Tcount", user="postgres", password="pass", hos
 
 
 cur = conn.cursor()
-# cur.execute('''CREATE TABLE Tweetwordcount
-#        (word TEXT PRIMARY KEY     NOT NULL,
-#        count INT     NOT NULL);''')
-# conn.commit()
-# conn.close()
 
 class WordCounter(Bolt):
 
